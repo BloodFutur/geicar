4f0ab1278dc162fb5d45a47fc4fce00dc35e390b
Tang Huong Cam <thc18092002@gmail.com>
2024-11-20 14:55:41 +0100

    Modify period update command and gain value of regulation loop

--- raspberryPI3/ros2_ws/src/car_control/include/car_control/car_control_node.h
+++ raspberryPI3/ros2_ws/src/car_control/include/car_control/car_control_node.h
@@ -5,13 +5,11 @@
 #include <string.h>  
 
 
-#define PERIOD_UPDATE_CMD 1ms //Period to update proupulsion and steering command in [ms]
+#define PERIOD_UPDATE_CMD 100ms //Period to update proupulsion and steering command in [ms]
 
 #define STOP 50 //PWM value to stop motors
-#define KP_LEFT 0.1109
-#define KI_LEFT 0.0
-#define KP_RIGHT 0.1097
-#define KI_RIGHT 0.0
+#define KPI_LEFT 0.1109
+#define KPI_RIGHT 0.1097
 
 
 #endif /*__ car_control_node_H */
