43cfdc2ddeaf3f7b7ed21a2a7e4bbd52c02b9716
kekoleko123 <eztallot@gmail.com>
2024-11-26 13:33:23 +0000

    changed setup file

--- raspberryPI3/ros2_ws/src/detection/setup.py
+++ raspberryPI3/ros2_ws/src/detection/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'obstacles_detection = detection.obstacles_detection:main',
+            'obstacles_detection_node = detection.obstacles_detection_node:main',
         ],
     },
 )
