61f80546a246dbae50a73d6848691a10faf36f75
Louayarfaoui95 <arfaouilouay1920@gmail.com>
2024-12-04 17:13:19 +0100

    Added ML and AI libraries in the dev container ROS2 env

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,6 +2,8 @@
 
 sudo apt update && \
 sudo apt install ros-humble-rviz2 -y && \
+pip3 install --upgrade pip && \
+pip3 install numpy scipy pandas scikit-learn tensorflow torch ultralytics opencv-python && \
 cd raspberryPI3/ros2_ws && \
 colcon build --packages-select interfaces && \
 colcon build --packages-skip interfaces && \
