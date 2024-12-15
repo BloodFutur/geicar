cec79ab78f6231096dbac780e9f85138041b5d1f
Tang Huong Cam <thc18092002@gmail.com>
2024-12-15 11:55:39 +0100

    feat-48: Add robot_localization and navsat_transform node in geicar_start package

--- raspberryPI3/ros2_ws/src/geicar_start/launch/geicar.launch.py
+++ raspberryPI3/ros2_ws/src/geicar_start/launch/geicar.launch.py
@@ -13,7 +13,7 @@ def generate_launch_description():
         emulate_tty=True
     )
 
-    joystick_to_cmd_node = Node(
+    joystick_to_cmd_node = Node( 
         package="joystick",
         executable="joystick_to_cmd",
         emulate_tty=True
@@ -70,6 +70,29 @@ def generate_launch_description():
         emulate_tty=True
     )
 
+    localization_config_dir = os.path.join(get_package_share_directory('geicar_start'), 'config')
+
+    robot_localization_node = Node(
+        package='robot_localization', 
+        executable='ekf_node', 
+        name='ekf_filter_node_odom',
+        output='screen',
+        parameters=[os.path.join(localization_config_dir, 'ekf_config.yaml')],
+        remappings=[('odometry/filtered', 'odometry/local')]
+    ) 
+
+    navsat_transform_node = Node(
+        package='robot_localization', 
+        executable='navsat_transform_node', 
+        name='navsat_transform',
+        output='screen',
+        parameters=[os.path.join(localization_config_dir, 'ekf_test.yaml')],
+        remappings=[('imu/data', '/imu/modified_frame_id'),
+                    ('gps/fix', 'gps/fix'), 
+                    ('gps/filtered', 'gps/filtered'),
+                    ('odometry/gps', 'odometry/gps')]           
+    )   
+
     ld.add_action(joystick_node)
     ld.add_action(joystick_to_cmd_node)
     ld.add_action(can_rx_node)
@@ -80,5 +103,7 @@ def generate_launch_description():
     ld.add_action(system_check_node)
     ld.add_action(rosbridge_server_node)
     ld.add_action(obstacles_detection_node)
+    ld.add_action(robot_localization_node)
+    ld.add_action(navsat_transform_node)
 
     return ld
