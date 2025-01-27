0f6060fdc61b4d888a589c89acdba92749520075
Louayarfaoui95 <arfaouilouay1920@gmail.com>
2025-01-27 13:45:06 +0100

    added verification for Detection FP

--- jetsonNano/ros2_ws/src/usb_cam/scripts/LPD_Pipeline.py
+++ jetsonNano/ros2_ws/src/usb_cam/scripts/LPD_Pipeline.py
@@ -168,6 +168,8 @@ class PlateDetection(Node):
                         extracted_text += ocr_result[0][1]  
                         confidence.append(ocr_result[0][2])
                 self.get_logger().info(f"Extracted Text: {extracted_text}")
+                if extracted_text=="":
+                    return
             else:
                 self.get_logger().info("No license plate detected.")
                 extracted_text="0000000"
