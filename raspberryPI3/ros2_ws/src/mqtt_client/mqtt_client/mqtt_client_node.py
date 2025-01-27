23f8b3e8ba0175cde4254236ce4cfaa712b908fc
Ronan Bonnet <ronan.bonnet42@wanadoo.fr>
2025-01-27 17:35:01 +0000

    Fix typo in MessageHandler

--- raspberryPI3/ros2_ws/src/mqtt_client/mqtt_client/mqtt_client_node.py
+++ raspberryPI3/ros2_ws/src/mqtt_client/mqtt_client/mqtt_client_node.py
@@ -25,7 +25,7 @@ class MessageHandler:
     @staticmethod
     def to_dict(obj: Any) -> Dict:
         if isinstance(obj, dict):
-            return dict((key.lstrip("_"), MessageHandler.todict(val)) for key, val in obj.items())
+            return dict((key.lstrip("_"), MessageHandler.to_dict(val)) for key, val in obj.items())
         elif hasattr(obj, "_ast"):
             return todict(obj._ast())
         elif hasattr(obj, "__iter__") and not isinstance(obj, str):
